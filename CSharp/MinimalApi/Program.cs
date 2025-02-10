2b21d29ec99af7343ba330106f10ad4e2a23e3b9
Fujimoto Shigeo <fujimoto.shigeo@donuts.ne.jp>
2025-02-10 15:37:51 +0900

    add:csharp GET with parameters

--- CSharp/MinimalApi/Program.cs
+++ CSharp/MinimalApi/Program.cs
@@ -5,6 +5,8 @@ app.MapGet("/", () => "Root Hello World!");
 
 app.MapGet("/api/hello", () => "Hello World!");
 
-app.MapPost("/api/hello", (string name) => $"Hello {name}!");
+app.MapGet("/api/hello/{name}", (string name) => $"Hello {name}! From GET");
+
+app.MapPost("/api/hello", (string name) => $"Hello {name}! From POST");
 
 app.Run();
